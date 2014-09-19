d27494c90e8aa5d67fde4b58046de5602d165c4a
Edgar Fajardo <efajardo@physics.ucsd.edu>
2014-09-19 18:45:14 +0000

    Passed the new arguments, key, usenrame and goc to the esmondCaller
    git-svn-id: https://vdt.cs.wisc.edu/svn/software/rsv/trunk@19768 4e558342-562e-0410-864c-e07659590f8c

--- rsv-perfsonar/libexec/probes/worker-scripts/esmondCaller.sh
+++ rsv-perfsonar/libexec/probes/worker-scripts/esmondCaller.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-host=$1 start=$2 scl enable python27 - << \EOF
+host=$1 start=$2 username=$3 key=$4 goc=$5 scl enable python27 - << \EOF
 echo $home
 source ./esmond.env
-python esmonduploader/caller.py -s $start -u $host -p
+python esmonduploader/caller.py -s $start -u $host -p -w $username -k $key -g $goc
 EOF
 
